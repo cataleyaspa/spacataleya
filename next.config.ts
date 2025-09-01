2fa858aaf15ad5e3d672bef174696e8dd5d8a79c
Emerson Alarcon <cataleyaaspaa@gmail.com>
2025-09-01 00:43:29 -0500

    paguina

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
+  output: "export", // 👈 esto reemplaza a `next export`
   images: {
-    unoptimized: true,
+    unoptimized: true, // necesario para GitHub Pages
   },
-  typescript: {
-    // ignoreBuildErrors: true,
-  },
+  basePath: "/spacataleya", // 👈 nombre de tu repo (ajústalo)
+  assetPrefix: "/spacataleya/", // 👈 importante para rutas correctas
 };
 
 export default nextConfig;
